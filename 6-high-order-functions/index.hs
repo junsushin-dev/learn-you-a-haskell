0019f26a3dc4e0e05441068ed95b47e2a2ff6b7b
junsushin-dev <junsushin4546@gmail.com>
2021-09-21 21:53:38 +0900

    feat: Add exercises for 6-3 ~ 6-7 in high order functions

--- 6-high-order-functions/index.hs
+++ 6-high-order-functions/index.hs
@@ -33,3 +33,74 @@ filter' _ [] = []
 filter' p (x:xs)
   | p x = x : filter p xs
   | otherwise = filter p xs
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+  where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n:chain (n `div` 2)
+  | odd n = n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..1000]))
+  where isLong xs = length xs > 15
+
+listOfFuns :: [Integer -> Integer]
+listOfFuns = map (*) [0..]
+
+applyWith :: [a -> b] -> a -> [b]
+applyWith [] _ = []
+applyWith (x:xs) y = x y: applyWith xs y
+
+-- Lambdas
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f = \x y -> f y x
+
+-- folds
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y = foldl (\acc x -> if x == y then True else acc) False
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f = foldr (\x acc -> f x : acc) []
+
+sum'' :: (Num a) => [a] -> a
+sum'' = foldl1 (+)
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+product' :: (Num a) => [a] -> a
+product' = foldr1 (*)
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\x acc -> if p x then x:acc else acc) []
+
+head' :: [a] -> a
+head' = foldr1 (\x _ -> x)
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+-- $, . function
+fn = ceiling . negate . tan . cos . max 50
+
+oddSquareSum :: Integer
+oddSquareSum = 
+  let oddSquares = filter odd $ map (^2) [1..]
+      belowLimit = takeWhile (<10000) oddSquares
+  in sum belowLimit
