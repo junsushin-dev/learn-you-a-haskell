76de46f421cc52fe06f5df8927378146fdc24fbf
junsushin-dev <junsushin4546@gmail.com>
2021-09-24 22:57:52 +0900

    feat: Add exercises for 7-3 Data.Char

--- 7-modules/index.hs
+++ 7-modules/index.hs
@@ -1,5 +1,7 @@
 import Data.List
+import Data.Char
 
+-- Data.List
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
 
@@ -8,3 +10,12 @@ search needle haystack =
   let nlen = length needle
   in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
   
+-- Data.Char
+encode :: Int -> String -> String
+encode shift msg =
+  let ords = map ord msg
+      shifted = map (+ shift) ords
+  in  map chr shifted
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
