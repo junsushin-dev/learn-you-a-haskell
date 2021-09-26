a918c806bc9b01dcc1e5e8c3d7aa15457a4fe9ea
junsushin-dev <junsushin4546@gmail.com>
2021-09-26 23:21:25 +0900

    feat: Add exercises for 7-4 Data.Map

--- 7-modules/index.hs
+++ 7-modules/index.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Char
+import qualified Data.Map as Map
 
 -- Data.List
 numUniques :: (Eq a) => [a] -> Int
@@ -19,3 +20,24 @@ encode shift msg =
 
 decode :: Int -> String -> String
 decode shift msg = encode (negate shift) msg
+
+-- Data.Map
+phoneBook =   
+    [("betty","555-2938")  
+    ,("betty","342-2492")  
+    ,("bonnie","452-2928")  
+    ,("patsy","493-2928")  
+    ,("patsy","943-2929")  
+    ,("patsy","827-9162")  
+    ,("lucille","205-2928")  
+    ,("wendy","939-8282")  
+    ,("penny","853-2492")  
+    ,("penny","555-2111")  
+    ] 
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
+phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
+
