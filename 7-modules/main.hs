12851cd3004d115920e560a77c29c3d8c04698bf
junsushin-dev <junsushin4546@gmail.com>
2021-09-27 22:07:58 +0900

    feat: Add exercises for 7-6 custom modules

---
+++ 7-modules/main.hs
@@ -0,0 +1,8 @@
+import qualified Geometry.Sphere as Sphere  
+import qualified Geometry.Cuboid as Cuboid  
+import qualified Geometry.Cube as Cube  
+
+sphereVolume = Sphere.volume 3
+sphereArea = Sphere.area 3
+cubeVolume = Cube.volume 3
+cubeArea = Cube.area 3
