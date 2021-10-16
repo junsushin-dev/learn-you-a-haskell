82723a15d7701bd5bdeb4c7d98fed298f4b6a26b
junsushin-dev <junsushin4546@gmail.com>
2021-10-16 23:26:42 +0900

    feat: Add exercises for 9-4 Randomness

---
+++ 9-input-output/random/guessNumber.hs
@@ -0,0 +1,18 @@
+import System.Random
+import Control.Monad(when)
+
+main = do
+    gen <- getStdGen
+    askForNumber gen
+
+askForNumber :: StdGen -> IO ()
+askForNumber gen = do
+    let (randNumber, newGen) = randomR (1,10) gen :: (Int, StdGen)
+    putStrLn "Which number in the range fro 1 to 10 am I thinking of? "
+    numberString <- getLine
+    when (not $ null numberString) $ do
+        let number = read numberString
+        if randNumber == number
+            then putStrLn "You guessed it!"
+            else putStrLn $ "Sorry, it was " ++ show randNumber
+        askForNumber newGen
