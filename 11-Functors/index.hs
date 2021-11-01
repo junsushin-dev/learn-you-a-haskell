fc59c6405e77ed766fee738cdad6ddca42ccf5bb
junsushin-dev <junsushin4546@gmail.com>
2021-11-01 23:21:22 +0900

    feat: Finish exercises for 11-1 Functors redux

--- 11-Functors/index.hs
+++ 11-Functors/index.hs
@@ -14,3 +14,9 @@ import Data.List
 main = do
   line <- fmap (intersperse '-' . reverse . map toUpper) getLine 
   putStrLn line
+
+data CMaybe a = CNothing | CJust Int a deriving (Show)
+
+instance Functor CMaybe where
+  fmap f CNothing = CNothing
+  fmap f (CJust counter x) = CJust (counter + 1) (f x)
