aa1b6d8b0c79b145e4ef680a7e91e6083c01af09
junsushin-dev <junsushin4546@gmail.com>
2021-11-03 23:46:28 +0900

    feat: Add some more exercises for 11-2 Applicative Functors

--- 11-Functors/applicative.hs
+++ 11-Functors/applicative.hs
@@ -1,3 +1,4 @@
+import System.Directory.Internal.Prelude (Applicative)
 -- myAction :: IO String
 -- myAction = do
 --   a <- getLine
@@ -10,3 +11,6 @@ myAction = (++) <$> getLine <*> getLine
 main = do
   a <- myAction
   putStrLn $ "The tow lines concatenated turn out to be: " ++ a
+
+liftA2 :: (Applicative f) => (a -> b -> c) -> f a -> f b -> f c
+liftA2 f a b = f <$> a <*> b
