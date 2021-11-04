0e8bbf0111b0ed43e1cb46d77bda1d2ff9c85960
junsushin-dev <junsushin4546@gmail.com>
2021-11-04 23:11:01 +0900

    feat: Finish exercises for 11-2 Applicative Functors

--- 11-Functors/applicative.hs
+++ 11-Functors/applicative.hs
@@ -14,3 +14,11 @@ main = do
 
 liftA2 :: (Applicative f) => (a -> b -> c) -> f a -> f b -> f c
 liftA2 f a b = f <$> a <*> b
+
+-- sequenceA :: (Applicative f) => [f a] -> f [a]
+-- sequenceA [] = pure []
+-- sequenceA (x:xs) = (:) <$> x <*> sequenceA xs
+
+sequenceA :: (Applicative f) => [f a] -> f [a]
+sequenceA = foldr (liftA2 (:)) (pure [])
+
