3c616096406f223dd317e82128074d7fc6455187
junsushin-dev <junsushin4546@gmail.com>
2021-11-18 21:38:57 +0900

    feat: Finish exercises for 13-1 Writer

--- 13-Monad2/writer.hs
+++ 13-Monad2/writer.hs
@@ -9,3 +9,50 @@ multWithLog = do
   b <- logNumber 5
   tell ["Gonna multiply these two"]
   return (a*b)
+
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Semigroup (DiffList a) where
+  DiffList f <> DiffList g = DiffList (\xs -> f (g xs))
+instance Monoid (DiffList a) where
+  mempty = DiffList (\xs -> [] ++ xs)
+  (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
+
+gcd' :: Int -> Int -> Writer (DiffList String) Int
+gcd' a b
+  | b == 0 = do
+    tell (toDiffList ["Finished with " ++ show a])
+    return a
+  | otherwise = do
+    tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+    gcd' b (a `mod` b)
+
+gcdReverse :: Int -> Int -> Writer (DiffList String) Int
+gcdReverse a b
+  | b == 0 = do
+    tell (toDiffList ["Finished with " ++ show a])
+    return a
+  | otherwise = do
+    result <- gcdReverse b (a `mod` b)
+    tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+    return result
+
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+  tell (toDiffList ["0"])
+finalCountDown x = do
+  finalCountDown (x-1)
+  tell (toDiffList [show x])
+
+finalCountDownSlow :: Int -> Writer [String] ()
+finalCountDownSlow 0 = do
+  tell ["0"]
+finalCountDownSlow x = do
+  finalCountDownSlow (x-1)
+  tell [show x]
