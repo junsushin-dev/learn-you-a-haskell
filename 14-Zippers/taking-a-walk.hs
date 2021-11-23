07b95ded43b3d3f22accbd17afb07a02266c526d
junsushin-dev <junsushin4546@gmail.com>
2021-11-23 09:55:21 +0900

    feat: Add exercises for 14-2 A trail of breadcrumbs

--- 14-Zippers/taking-a-walk.hs
+++ 14-Zippers/taking-a-walk.hs
@@ -39,3 +39,32 @@ elemAt :: Directions -> Tree a -> a
 elemAt (L:ds) (Node _ l _) = elemAt ds l
 elemAt (R:ds) (Node _ _ r) = elemAt ds r
 elemAt [] (Node x _ _) = x
+
+type BreadCrumbs a = [Crumb a]
+
+goLeft :: (Tree a, BreadCrumbs a) -> (Tree a, BreadCrumbs a)
+goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
+
+goRight :: (Tree a, BreadCrumbs a) -> (Tree a, BreadCrumbs a)
+goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
+
+goUp :: (Tree a, BreadCrumbs a) -> (Tree a, BreadCrumbs a)
+goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
+goUp (t, RightCrumb x l:bs) = (Node x l t, bs)
+
+x -: f = f x
+
+data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving (Show)
+
+type Zipper a = (Tree a, BreadCrumbs a)
+
+modify :: (a -> a) -> Zipper a -> Zipper a
+modify f (Node x l r, bs) = (Node (f x) l r, bs)
+modify f (Empty, bs) = (Empty, bs)
+
+attach :: Tree a -> Zipper a -> Zipper a
+attach t (_, bs) = (t, bs)
+
+topMost :: Zipper a -> Zipper a
+topMost (t,[]) = (t,[])
+topMost z = topMost (goUp z)
